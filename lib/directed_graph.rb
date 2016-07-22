40a52399550a2224fa9b690e6a22e1d40437fa8c
Aashil Desai <aashildesai@SKs-MacBook-Pro.local>
2016-07-22 15:54:47 -0400

    wrote some more test cases

--- lib/directed_graph.rb
+++ lib/directed_graph.rb
@@ -10,7 +10,7 @@ class DirectedGraph
 
 	#returns array of all of ther vertex values
 	def vertices
-		return @vertices
+		# return @vertices
 	end
 
 	#returns an array of all of the out neighbors of a vertex
@@ -43,7 +43,7 @@ class DirectedGraph
 	end
 
 	#returns true iff vertex1.neighbors.contains?(vertex2) || vertex2.neighbors.contains?(vertex1)
-	def neighbors?(vertex1, vertex2)
+	def has_edge?(vertex1, vertex2)
 	end
 
 	#returns the number of vertices in the graph
@@ -55,25 +55,25 @@ class DirectedGraph
 	#adds a vertex to the graph
 	#if a vertex is already in the graph, does nothing
 	#returns true iff a vertex was added (false if the vertex was already in the graph)
-	def add_vertex(vertex)
+	def add_vertex!(vertex)
 	end
 
 	#adds an edge with the given weight between two vertices, starting at vertex1 and pointing to the vertex2
 	#if there is already an edge with said weight, it updates the weight
 	#if either one (or both) of the vertices are not in the graph, it throws an ArgumentError
-	def add_edge(vertex1, vertex2, weight)
+	def add_edge!(vertex1, vertex2, weight)
 	end
 
 	#removes a vertex and and edges associated with it
 	#if there is no such vertex in the graph, throws an ArgumentError
 	#returns the removed vertex
-	def remove_vertex(vertex)
+	def remove_vertex!(vertex)
 	end
 
 	#removes an edge between two vertices (vertex1 -> vertex2)
 	#returns the weight of the removed edge
 	#if there was no such edge (or either one of the vertices were not in the vertex, throws an ArgumentError)
-	def remove_edge(vertex1, vertex2)
+	def remove_edge!(vertex1, vertex2)
 	end
 
 end
