81d7ba307e97ba5c18b51e6850687d768f634a24
Aashil Desai <aashildesai@SKs-MacBook-Pro.local>
2016-07-25 10:47:29 -0400

    implemented add_edge, add_vertex, has_edge, has_vertex, weight, vertices, and size methods

--- lib/directed_graph.rb
+++ lib/directed_graph.rb
@@ -10,7 +10,7 @@ class DirectedGraph
 
 	#returns array of all of ther vertex values
 	def vertices
-		# return @vertices
+		@vertices.keys
 	end
 
 	#returns an array of all of the out neighbors of a vertex
@@ -28,6 +28,13 @@ class DirectedGraph
 	#returns the weight of the edge between two vertices (vertex1 -> vertex2)
 	#if there is no edge throws and ArgumentError
 	def weight(vertex1, vertex2)
+		if !has_vertex?(vertex1) || !has_vertex?(vertex2)
+			raise ArgumentError.new('Given vertex does not exist in graph')
+		elsif !has_edge?(vertex1, vertex2)
+			raise ArgumentError.new('Given edge does not exist in graph')
+		end
+
+		return @vertices[vertex1][vertex2]
 	end
 
 	#given a vertex v, returns a vertex w if weight(v, w) is the largest for all w defined for v
@@ -39,15 +46,22 @@ class DirectedGraph
 	end
 
 	#returns true iff this.vertices.contains?(vertex)
-	def contains?(vertex)
+	def has_vertex?(vertex)
+		@vertices.has_key?(vertex)
 	end
 
 	#returns true iff vertex1.neighbors.contains?(vertex2) || vertex2.neighbors.contains?(vertex1)
 	def has_edge?(vertex1, vertex2)
+		if !has_vertex?(vertex1) || !has_vertex?(vertex2)
+			raise ArgumentError.new('Given vertex does not exist in graph')
+		end
+
+		@vertices[vertex1].has_key?(vertex2)
 	end
 
 	#returns the number of vertices in the graph
 	def size
+		@vertices.size
 	end
 
 	#mutator methods:
@@ -56,12 +70,31 @@ class DirectedGraph
 	#if a vertex is already in the graph, does nothing
 	#returns true iff a vertex was added (false if the vertex was already in the graph)
 	def add_vertex!(vertex)
+		if has_vertex?(vertex)
+			return false
+		else
+			@vertices[vertex] = {}
+			return true
+		end
 	end
 
 	#adds an edge with the given weight between two vertices, starting at vertex1 and pointing to the vertex2
 	#if there is already an edge with said weight, it updates the weight
 	#if either one (or both) of the vertices are not in the graph, it throws an ArgumentError
 	def add_edge!(vertex1, vertex2, weight)
+		if !has_vertex?(vertex1) || !has_vertex?(vertex2)
+			raise ArgumentError.new('Given vertex does not exist in graph')
+		end
+
+		if has_edge?(vertex1, vertex2)
+			ehash = @vertices[vertex1]
+			ehash[vertex2] = weight
+			return false
+		else #need to make a new edge
+			ehash = @vertices[vertex1]
+			ehash[vertex2] = weight
+			return true
+		end
 	end
 
 	#removes a vertex and and edges associated with it
--- spec/directed_graph_spec.rb
+++ spec/directed_graph_spec.rb
@@ -1,1 +1,104 @@
 #spec/directed_graph_spec.rb
+
+require "directed_graph"
+
+describe DirectedGraph do
+
+	#First, accessor methods on an empty graph (to set a good base)
+	describe "empty graph" do
+		blank = DirectedGraph.new()
+
+		describe ".vertices" do
+			context "given an empty graph" do
+				it "returns an empty hash" do
+					expect(blank.vertices().length()).to eql(0)
+				end
+			end
+		end
+
+		describe ".size" do
+			context "given empty graph" do
+				it "size should be 0" do
+					expect(blank.size()).to eql(0)
+				end
+			end
+		end
+
+
+	end
+
+	#mutator methods first:
+	describe ".add_vertex!" do
+		context "given an empty graph" do
+			it "adds exactly one vertex" do
+				g = DirectedGraph.new()
+
+				expect(g.add_vertex!("Water")).to eql(true)
+				expect(g.size).to eql(1)
+				expect(g.has_vertex?("Water")).to eql(true)
+			end
+		end
+
+		context "graph with different element in it" do
+			it "adds the element" do
+				g = DirectedGraph.new()
+				g.add_vertex!("Water")
+
+				expect(g.add_vertex!("Fire")).to eql(true)
+				expect(g.size).to eql(2)
+				expect(g.has_vertex?("Fire")).to eql(true)
+				expect(g.has_vertex?("Water")).to eql(true)
+			end
+		end
+
+		context "given a graph with the same element in it" do
+			it "doesn't add the element" do
+				g = DirectedGraph.new()
+				g.add_vertex!("Water")
+
+				expect(g.add_vertex!("Water")).to eql(false)
+				expect(g.size).to eql(1)
+			end
+		end
+	end
+
+	describe ".add_edge!" do
+		context "given vertices not in the graph" do
+			it "throw an error" do
+				expect{DirectedGraph.new().add_edge!("Grass", "Electric", 3)}.to raise_error(ArgumentError)
+			end
+		end
+
+		context "given vertices without an edge between them" do
+			it "creates an edge pointing from one to the other" do
+				g = DirectedGraph.new()
+				g.add_vertex!("Dragon")
+				g.add_vertex!("Fairy")
+
+				expect(g.add_edge!("Dragon", "Fairy", 2)).to eql(true)
+				expect(g.has_edge?("Dragon", "Fairy")).to eql(true)
+				expect(g.weight("Dragon", "Fairy")).to eql(2)
+
+				expect(g.has_edge?("Fairy", "Dragon")).to eql(false)
+			end
+		end
+
+		context "given vertices with an edge already between them" do
+			it "updates the edge weight" do
+				g = DirectedGraph.new()
+				g.add_vertex!("Dragon")
+				g.add_vertex!("Fairy")
+				g.add_edge!("Dragon", "Fairy", 0)
+
+				expect(g.add_edge!("Dragon", "Fairy", 2)).to eql(false)
+				expect(g.has_edge?("Dragon", "Fairy")).to eql(true)
+				expect(g.weight("Dragon", "Fairy")).to eql(2)
+
+				expect(g.has_edge?("Fairy", "Dragon")).to eql(false)
+			end
+		end
+
+
+	end
+
+end
