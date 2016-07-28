b2f812a746b6ce412f1bad914f32324fad7e7a6a
Aashil Desai <aashildesai@SKs-MacBook-Pro.local>
2016-07-28 11:08:21 -0400

    Created Union Find

---
+++ lib/util/union_find.rb
@@ -0,0 +1,25 @@
+
+
+class UnionFind
+
+	#creates the Union Find datastructure
+	def initialize
+	end
+
+	#creates it and then includes all of the items
+	def initialize array_of_items
+	end
+
+	#gives you the root for that one item
+	def find item
+	end
+
+	#adds the item into the Union Find datastructure
+	def include item
+	end
+
+	#unions two separate items
+	def union itemA itemB
+	end
+
+end
